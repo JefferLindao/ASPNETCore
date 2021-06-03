557bbe00e512946fd83a2f9094961c4d148cf225
JefferLindao <jefferlindao@gmail.com>
2021-06-03 10:26:12 -0500

    Creando formaulario

--- Controllers/CursoController.cs
+++ Controllers/CursoController.cs
@@ -28,6 +28,19 @@ namespace PlatziASPNETCore.Controllers
       ViewBag.Fecha = DateTime.Now;
       return View("MultiplesCursos", _context.Cursos);
     }
+    public IActionResult Create()
+    {
+      ViewBag.Fecha = DateTime.Now;
+      return View();
+    }
+    [HttpPost]
+    public IActionResult Create(Curso curso)
+    {
+      ViewBag.Fecha = DateTime.Now;
+      _context.Cursos.Add(curso);
+      _context.SaveChanges();
+      return View();
+    }
     private EscuelaContext _context;
     public CursoController(EscuelaContext context)
     {
